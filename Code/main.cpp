caab5aea0c216f5a5a65c994b79048403fa965e1
Cyril <cyril.novel@gmail.com>
2014-04-22 22:55:27 +0200

    Wrote the cube processing function

--- Code/main.cpp
+++ Code/main.cpp
@@ -12,8 +12,39 @@
 #include "grid.h"
 #include "point.h"
 #include "cube.h"
+#include "code.h"
 
-float isofunction(Point p) {
+
+std::vector< std::vector <Point> > cubeProcessing(Cube c, std::vector< std::vector<int> > rotations) {
+	std::vector<int> markedVertices;
+	int nbPointsInside = 0;
+	bool swap = false;
+	for (int i = 0; i < c.listOfPoints.size(); i++) {
+		if insideModel(c.listOfPoints[i]) {
+			markedVertices.append(1);
+			nbPointsInside++;
+		} else {
+			markedVertices.append(0);
+		}
+	}
+
+	if nbPointsInside > 4: {
+		for (int i = 0; i < 8; i++) {
+			markedVertices[i] = (markedVertices[i] == 1)?0:1;
+		}
+	}
+
+	int code = -1
+
+	for (int i = 0; i < rotations.size(); i++) {
+		code = computeRotatedCode(markedVertices, rotations[i]);
+		if acceptable(code) {
+			return triangles(code, res, rotations, c)
+		}
+	}
+}
+
+bool insideModel(Point p, float isovalue = 0) {
 	//* Torus equation
 	float R = 2.0;
 	float r = 0.5;
@@ -25,9 +56,26 @@ float isofunction(Point p) {
 	float f = pow(p.x, 2.0) + pow(p.y, 2.0) + pow(p.z, 2.0) - pow(R, 2.0);
 	//*/
 
-	return f;
+	if f > isovalue {
+		return false;
+	} else {
+		return true;
+	}
 }
 
 int main(int argc, char** argv) {
-	
+	Point bottomPoint = Point(-4.0, -4.0, -4.0);
+	Point topPoint = Point(4.0, 4.0, 4.0);
+
+	float res = .2;
+	Grid g = Grid(topPoint, bottomPoint, res);
+
+	std::vector< std::vector <Point> > trianglesList;
+
+	for (int i = 0; i < g.listOfCubes.size(); i++) {
+		trianglesList.append(cubeProcessing(g.listOfCubes[i]));
+	}
+
+	int nbVertices = 3 * trianglesList.size();
+	int nbTriangles = trianglesList.size();
 }
