45cbbc8358200b0cc80cdfed1c800a359b2ea0f0
Cyril <cyril.novel@gmail.com>
2014-04-22 08:55:44 +0200

    Baseline of the code

---
+++ Code/main.cpp
@@ -0,0 +1,25 @@
+///////////////////////////////////
+// Marching cubes implementation //
+//     CC-BY 3.0 Cyril NOVEL     //
+///////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+float isofunction(Point p) {
+	//* Torus equation
+	float R = 2.0;
+	float r = 0.5;
+	float f = pow(R - sqrt(pow(p.x, 2.0) + pow(p.y, 2.0)), 2.0) + pow(p.z, 2.0) - pow(r, 2.0);
+	//*/
+	
+	return f;
+}
+
+int main(int argc, char** argv) {
+	
+}
